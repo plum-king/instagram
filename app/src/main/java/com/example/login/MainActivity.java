56b83cfd017acfce900859e4fb59afc47db79fd7
plum-king <jeesoo0908@gmail.com>
2021-11-23 03:18:11 +0900

    [mod] newFeed upload to firebase

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -10,17 +10,44 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
 
 import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    ImageButton plusBtn;
+    Button tempBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        plusBtn = (ImageButton)findViewById(R.id.plus_imgBtn);
+        //tempBtn = (Button)findViewById(R.id.temp_btn);
+        plusBtn.setOnClickListener(this);
+        //tempBtn.setOnClickListener(this);
+
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.plus_imgBtn:
+                startActivity(new Intent(MainActivity.this, NewFeedActivity.class));
+                break;
+//            case R.id.temp_btn:
+//                startActivity(new Intent(MainActivity.this, NewFeedActivity.class));
+//                break;
+        }
+    }
 }
