56e7b1a0fbe16a978f6e04d1a05a7ed50ed2dcd0
DESKTOP-NR7CBTO\user <listenyoon@gmail.com>
2021-12-31 22:24:15 +0900

    [feat] profile page switch
    by fragment and bottom navigation bar

---
+++ app/src/main/java/com/example/login/Fragment/ProfileFragment.java
@@ -0,0 +1,35 @@
+package com.example.login.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.login.EditProfileActivity;
+import com.example.login.R;
+
+public class ProfileFragment extends Fragment {
+    private View view;
+    Button editProfileButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        editProfileButton = view.findViewById(R.id.edit_profile);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -4,23 +4,50 @@
  */
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearSnapHelper;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.SnapHelper;
+import androidx.fragment.app.Fragment;
 
-import android.content.ClipData;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.MenuItem;
 
-import java.util.ArrayList;
+import com.example.login.Fragment.HomeFragment;
+import com.example.login.Fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment profileFragment, homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationSelectedListener);
+
+        profileFragment = new ProfileFragment();
+        homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+                    return true;
+                case R.id.nav_mypage:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profileFragment).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 
 }
