cdf9a665c96c234c088d77e6b15fe181fe502608
DESKTOP-NR7CBTO\user <listenyoon@gmail.com>
2022-01-02 15:29:01 +0900

    [del] Fragment files
    TODO : fix done and return error

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -4,56 +4,17 @@
  */
 package com.example.login;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.example.login.Fragment.HomeFragment;
-import com.example.login.Fragment.ProfileFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    BottomNavigationView bottomNavigationView;
-    Fragment selectedFragment = null;
-    Fragment profileFragment, homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationSelectedListener);
-
-        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-//                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.nav_mypage:
-                    Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-                    startActivity(intent);
-                    break;
-            }
-
-//            if(selectedFragment != null) {
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-//            }
-
-            return false;
-        }
-    };
 
 }
