c4e1cfe9f283f35154290cd09e5bf22d8f7725ec
gitLization <gitLization@gmail.com>
2022-01-02 16:14:55 +0900

    [login]

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -5,22 +5,56 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.SnapHelper;
 
-import android.content.ClipData;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Home";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+
+    //getUerInfo
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getImages();
+    }
+    private void getImages(){
+        Log.d(TAG, "InitImage Bitmaps.");
+        mImageUrls.add("https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg");
+        mNames.add("init");
+
+        initRecyclerView();
     }
 
+    private void initRecyclerView() {
+        Log.d(TAG, "Init Recycler View.");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+    }
 }
